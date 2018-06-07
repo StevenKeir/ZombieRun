e9ea33c7460e050bda24dc89bd0153a595816981
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2018-06-07 14:26:41 +0800

    Update - Steven 07/06/2018
    - Added animation
    - Updated Swiper script

---
+++ ZombieRun/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject playerIcon;
+
+    [Range(-1, 1)]
+    public int posInt = 0;
+    
+
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Swiper.Instance.IsSwiping(SwipeDirecetion.Left))
+        {
+            if(posInt > -1)
+            {
+                posInt--;
+            }
+        
+        }
+        if (Swiper.Instance.IsSwiping(SwipeDirecetion.Right))
+        {
+            if(posInt < 1)
+            {
+                posInt++;
+            }
+      
+        }
+
+        switch (posInt)
+        {
+            case -1:
+                playerIcon.transform.position = new Vector3(-3, 0, 0);
+                break;
+            case 0:
+                playerIcon.transform.position = new Vector3(0, 0, 0);
+                break;
+            case 1:
+                playerIcon.transform.position = new Vector3(3, 0, 0);
+                break;
+        }
+
+   
+
+	}
+}
---
+++ ZombieRun/Assets/Scripts/Swiper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SwipeDirecetion
+{
+    None = 0,
+    Left = 1,
+    Right = 2,
+    Up = 4,
+    Down = 8,
+}
+
+public class Swiper : MonoBehaviour {
+
+    private static Swiper instance;
+    public static Swiper Instance {get {return instance;}}
+
+
+
+    public SwipeDirecetion Direction { set; get; }
+    private Vector3 touchPos;
+    private float swipeResX = 5.0f;
+    private float swipeResY = 50.0f;
+
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        Direction = SwipeDirecetion.None;
+
+        if (Input.GetMouseButton(0))
+        {
+            touchPos = Input.mousePosition;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 deltaSwipe = touchPos - Input.mousePosition;
+
+            if(Mathf.Abs(deltaSwipe.x) > swipeResX)
+            {
+                //Swipe on the X axis
+                Direction |= (deltaSwipe.x < 0) ? SwipeDirecetion.Right : SwipeDirecetion.Left;
+            }
+
+            if (Mathf.Abs(deltaSwipe.y) > swipeResY)
+            {
+                //Swipe on the Y axis
+                Direction |= (deltaSwipe.x < 0) ? SwipeDirecetion.Up : SwipeDirecetion.Down;
+            }
+        }
+    }
+
+   
+        public bool IsSwiping(SwipeDirecetion dir)
+        {
+            return (Direction & dir) == dir;
+        }
+    }
+
+
+
+
