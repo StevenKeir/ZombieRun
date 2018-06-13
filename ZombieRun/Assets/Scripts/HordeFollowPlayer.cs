8918f07af037a5462d86543e209928cc75c73675
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2018-06-13 09:12:46 +0800

    update - 13/6/2018

--- ZombieRun/Assets/Scripts/HordeFollowPlayer.cs
+++ ZombieRun/Assets/Scripts/HordeFollowPlayer.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class HordeFollowPlayer : MonoBehaviour{
 
     public Transform player;
-    public float speed = 1.0f;
-    
-
+    public float speed = 0.08f;
 	
 	// Update is called once per frame
 	void Update () {
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position,player.position,step);
     }
+
+    /*
+    void SpeedIncrease ()
+    {
+        if (score > 40)
+        {
+            speed = 0.2f;
+        }
+        if (score > 30)
+        {
+            speed = 0.18f;
+        }
+        if (score > 20)
+        {
+            speed = 0.14f;
+        }
+        if (score > 10)
+        {
+            speed = 0.1f;
+        }
+        else speed = 0.08f;
+    }
+    */
 }
