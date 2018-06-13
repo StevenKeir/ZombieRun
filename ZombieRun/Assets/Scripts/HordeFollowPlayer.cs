c142e3506c316dee23711c44494316e70bf8409a
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2018-06-13 10:28:43 +0800

    update nathan 13/6

--- ZombieRun/Assets/Scripts/HordeFollowPlayer.cs
+++ ZombieRun/Assets/Scripts/HordeFollowPlayer.cs
@@ -7,26 +7,52 @@ public class HordeFollowPlayer : MonoBehaviour{
     private GameObject player;
     public float speed = 0.08f;
     public Score myScoreScript;
+    
 
     void Start()
     {
         speed = 0.08f;
         player = GameObject.FindGameObjectWithTag("Player");
-        myScoreScript = ;
+        myScoreScript = GameObject.FindGameObjectWithTag("Canvas").GetComponent<Score>();
     }
 
     // Update is called once per frame
     void Update () {
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position,player.transform.position,step);
+        SpeedIncrease();
     }
 
   
     void SpeedIncrease ()
     {
-        if (myScoreScript.scoreCounter > 40)
+        if (myScoreScript.scoreCounter > 100)
         {
-            speed = 0.2f;
+            speed = 0.46f;
+        }
+        else if (myScoreScript.scoreCounter > 90)
+        {
+            speed = 0.42f;
+        }
+        else if (myScoreScript.scoreCounter > 80)
+        {
+            speed = 0.38f;
+        }
+        else if (myScoreScript.scoreCounter > 70)
+        {
+            speed = 0.34f;
+        }
+        else if (myScoreScript.scoreCounter > 60)
+        {
+            speed = 0.3f;
+        }
+        else if (myScoreScript.scoreCounter > 50)
+        {
+            speed = 0.26f;
+        }
+        else if (myScoreScript.scoreCounter > 40)
+        {
+            speed = 0.22f;
         }
         else if (myScoreScript.scoreCounter > 30)
         {
